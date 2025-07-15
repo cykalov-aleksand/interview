49c9fe34415c0a836c404749506e40fe36c0c25e
1в <cykalov.aleksand@mail.ru>
2025-07-15 23:24:38 +0300

    проверьте домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args){
+        int[] numbers={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+        int variable;
+        System.out.println("Начальное состояние массива: "+ Arrays.toString(numbers));
+        for(int count=0;count<numbers.length/2;count++){
+            variable=numbers[count];
+            numbers[count]=numbers[numbers.length-1-count];
+            numbers[numbers.length-1-count]=variable;
+        }
+        System.out.println("Конечное значение массива: "+Arrays.toString(numbers));
     }
 }
